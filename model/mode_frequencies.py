e530d227627d7980ce220e35dd635a111750f2b1
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-11-08 23:58:06 +0200

    update slider view

---
+++ model/mode_frequencies.py
@@ -0,0 +1,4 @@
+class slider_data:
+    def __init__(self,slider_label):
+        self.slider_label = slider_label
+        
--- view/slider.py
+++ view/slider.py
@@ -13,17 +13,22 @@ class Slider(QWidget):
         self.slider_name = QLabel(name)
         self.slider_value = QLabel(f"{min_range_value}")
         self.slider_widget = QSlider(Qt.Orientation.Horizontal)
-        self.slider_widget.setRange(min_range_value,max_range_value)
+        self.slider_widget.setFixedWidth(200)
+        # self.slider_widget.setRange(min_range_value,max_range_value)
 
         # for 10 ticks
-        tick_interval = (max_range_value-min_range_value)//9
-        self.slider_widget.setTickInterval(tick_interval)
+        # tick_interval = (max_range_value-min_range_value)//9
+        # self.slider_widget.setTickInterval(tick_interval)
+
         self.slider_widget.setTickPosition(QSlider.TickPosition.TicksBelow) 
+        self.slider_widget.setRange(0,10)
+        self.slider_widget.setTickInterval(1)
+
         
         
         self.main_layout.addWidget(self.slider_name)
         self.main_layout.addWidget(self.slider_widget)
-        self.main_layout.addWidget(self.slider_value)
+        # self.main_layout.addWidget(self.slider_value)
         
         
 
