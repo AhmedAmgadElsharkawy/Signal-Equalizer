6c8cb4cf374877b284ae35f9a1c7a44f90886275
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-11-11 01:48:14 +0200

    initializing the work for spetro. and freq.

--- controller/frequency_domain_controller.py
+++ controller/frequency_domain_controller.py
@@ -1,4 +1,8 @@
 class FrequencyDomainController:
     def __init__(self,main_window):
         self.main_widnow = main_window
+
+    def plot_freq_domain(self):
+        # you need to clear your plot first
+        pass
         
