775d3327376583b916c001ecd0bbd1e6f2578879
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-11-11 13:34:48 +0200

    adding the limits to the input & output plots and fixing main_window naming error

--- controller/playback_buttons_controller.py
+++ controller/playback_buttons_controller.py
@@ -6,38 +6,40 @@ from scipy.io import wavfile
 import sounddevice as sd
 class PlaybackButtonsController:
     def __init__(self,main_window):
-        self.main_widnow = main_window
+        self.main_window = main_window
 
     def loadSignal(self):
-        file_path, _ = QFileDialog.getOpenFileName(self.main_widnow, "Open .wav file", "", "Audio Files (*.wav)")
+        file_path, _ = QFileDialog.getOpenFileName(self.main_window, "Open .wav file", "", "Audio Files (*.wav)")
         if file_path:
-            self.main_widnow.file_path = file_path
-            self.main_widnow.signal.calculate_data(file_path)
+            self.main_window.file_path = file_path
+            self.main_window.signal.calculate_data(file_path)
             self.plot_the_signal()
             self.set_all_sliders_to_one()
+            self.main_window.input_cine_signal_viewer.cine_signal_plot.setLimits(xMin=0, xMax=self.main_window.signal.time[-1])
+            self.main_window.output_cine_signal_viewer.cine_signal_plot.setLimits(xMin=0, xMax=self.main_window.signal.time[-1])
 
     def clearSignal(self):
-        self.main_widnow.signal.clear_signal()
+        self.main_window.signal.clear_signal()
         
-        self.main_widnow.input_cine_signal_viewer.cine_signal_plot.clear()
-        self.main_widnow.output_cine_signal_viewer.cine_signal_plot.clear()
-        self.main_widnow.frequency_domain_viewer.frequency_domain_plot.clear()
+        self.main_window.input_cine_signal_viewer.cine_signal_plot.clear()
+        self.main_window.output_cine_signal_viewer.cine_signal_plot.clear()
+        self.main_window.frequency_domain_viewer.frequency_domain_plot.clear()
         
 
     def plot_the_signal(self):
-        self.main_widnow.input_cine_signal_viewer.cine_signal_plot.clear()
-        self.main_widnow.output_cine_signal_viewer.cine_signal_plot.clear()
-        self.main_widnow.frequency_domain_viewer.frequency_domain_plot.clear()
-        self.main_widnow.frequency_domain_controller.plot_freq_domain()
-        self.main_widnow.spectrogram_controller.plot_spectrogram()
-        self.main_widnow.input_cine_signal_viewer.cine_signal_plot.plot(self.main_widnow.signal.time, self.main_widnow.signal.data, pen=pg.mkPen(color=(170, 0, 0)))
-        self.main_widnow.output_cine_signal_viewer.cine_signal_plot.plot(self.main_widnow.signal.time, self.main_widnow.signal.modified_data, pen=pg.mkPen(color=(170, 0, 0)))
-        self.main_widnow.frequency_domain_viewer.frequency_domain_plot.plot(self.main_widnow.signal.freqs, self.main_widnow.signal.magnitudes, pen=pg.mkPen(color=(170, 0, 0)))
+        self.main_window.input_cine_signal_viewer.cine_signal_plot.clear()
+        self.main_window.output_cine_signal_viewer.cine_signal_plot.clear()
+        self.main_window.frequency_domain_viewer.frequency_domain_plot.clear()
+        self.main_window.frequency_domain_controller.plot_freq_domain()
+        self.main_window.spectrogram_controller.plot_spectrogram()
+        self.main_window.input_cine_signal_viewer.cine_signal_plot.plot(self.main_window.signal.time, self.main_window.signal.data, pen=pg.mkPen(color=(170, 0, 0)))
+        self.main_window.output_cine_signal_viewer.cine_signal_plot.plot(self.main_window.signal.time, self.main_window.signal.modified_data, pen=pg.mkPen(color=(170, 0, 0)))
+        self.main_window.frequency_domain_viewer.frequency_domain_plot.plot(self.main_window.signal.freqs, self.main_window.signal.magnitudes, pen=pg.mkPen(color=(170, 0, 0)))
 
     def set_all_sliders_to_one(self):
         # Loop through all widgets in sliders_widget_layout
-        for i in range(self.main_widnow.sliders_widget_layout.count()):
-            widget = self.main_widnow.sliders_widget_layout.itemAt(i).widget()
+        for i in range(self.main_window.sliders_widget_layout.count()):
+            widget = self.main_window.sliders_widget_layout.itemAt(i).widget()
             
             # Check if the widget is an instance of your custom Slider class
             if isinstance(widget, Slider):
