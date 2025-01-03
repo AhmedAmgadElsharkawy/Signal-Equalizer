171a10e079d53c3a694aeac1c567cbb83eea7e23
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2025-01-03 04:30:25 +0200

    removing letter A

--- model/mode_frequencies.py
+++ model/mode_frequencies.py
@@ -10,10 +10,14 @@ class slider_data:
 mode_sliders_data = {
     "Uniform Range":[],
 
-    "Musical Instruments":[slider_data("bass",[(0,550)]),slider_data("Guitar",[(0,1000)]),slider_data("S",[(3500,11700)])],
+    "Musical Instruments":[slider_data("bass",[(0,550)]),slider_data("Guitar",[(0,1000)]),slider_data("S",[(3500,11700)]),
+                            slider_data("A", [(300, 400), (550, 750), (800, 1000), (1300, 1500), (1600, 1800)]), 
+                            slider_data("U", [(350, 600), (800, 1000), (1200, 1500), (1800, 1900), (4000, 5000)]),
+                            slider_data("I", [(800, 950), (1350, 1450), (1600, 1700), (1100, 1150)])],
 
     "Animal Sounds":[slider_data("owl",[(1000,1200)]),slider_data("frog",[(1200,1400)]),
-                    slider_data("canary",[(1400,1600)]),slider_data("insect",[(6000,20000)])],
+                    slider_data("canary",[(1400,1600)]),slider_data("insect",[(6000,20000)]), slider_data("Cat", [(600, 620), (1100, 1200), (1700, 2160), (4500, 6000)]),
+                    slider_data("Bat", [(6000, 20000)]), slider_data("Bird", [(2600, 4500)])],
 
     "ECG Abnormalities":[slider_data("normal",[(0,150)]),slider_data("Aflut",[(40,60)]),
                     slider_data("Vt",[(95,155)]),slider_data("Afib",[(60,95)])]
